21f7f4721e1f750730a2323c22075bd3ac032491
Brianna Buckner <bucknerb@cat.pdx.edu>
2013-05-21 18:29:14 -0700

    begin implementing mergesort

--- algorithms/mergesort.py
+++ algorithms/mergesort.py
@@ -1,2 +1,19 @@
-def mergesort.py():
-    return
+def merge(left, right):
+    return left.extend(right) # TODO: implement me
+
+def mergesort(array):
+    length = len(array)
+    mid = int(length / 2)
+    if length <= 1:
+        return array
+    # this is black magic and stands in place of n assignments
+    left = array[0:mid]
+    right = array[mid+1:length]
+    left = mergesort(left)
+    right = mergesort(right)
+    return merge(left, right)
+
+
+array = [4, 9, 10, 44, 13, 1, 7]
+mergesort(array)
+print(array)
