29c82fed0e888d40f2f66775f0b938f636fd50fd
Brianna Buckner <bucknerb@cat.pdx.edu>
2013-05-23 20:24:52 -0700

    begin testing functions

--- testing/testing.py
+++ testing/testing.py
@@ -1,13 +1,35 @@
-def truerandom(size):
+import os
+import time
+def truerandom(size, sort):
+    array = []
+    # open file and read $size lines
+    of = open("../4.txt", "r")
+    for i in range(0,size):
+        array.append(of.readline())
+    of.close()
+    # start timer and run over $sort algorithm
+    start = time.time()
+    if sort == "mergesort":
+        mergesort(array)
+    elif sort == "quicksort":
+        quicksort(array)
+    else:
+        heapsort(array)
+    # stop timer and save time to results file
+    stop = time.time()
+    of = open("truerandom.result", "a")
+    of.write(sort, stop - start)
 
+def reversesort(size, sort):
+    pass
 
-def reversesort(size):
+def nearsorted(size, sort):
+    pass
 
+def duplicates(size, sort):
+    pass
 
-def nearsorted(size):
+def sorted(size, sort):
+    pass
 
-
-def duplicates(size):
-
-
-def sorted(size):
+truerandom(40, "mergesort")
