25792f98052995b3091101f10cec6c3264f5bd57
Brianna Buckner <bucknerb@cat.pdx.edu>
2013-05-24 15:35:22 -0700

    fix mergesort so it's not O(n^2)

--- algorithms.py
+++ algorithms.py
@@ -1,18 +1,14 @@
 # merge function for second half of mergesort
 def merge(left, right):
     merged_list = []
-    l_len = len(left)
-    r_len = len(right)
-    while (l_len > 0 and r_len > 0):
-        if left[0] <= right[0]:
-            merged_list.append(left[0])
-            left = left[1:]
-            l_len = l_len - 1
+    left.reverse()
+    right.reverse()
+    while left and right:
+        if left[-1] <= right[-1]:
+            merged_list.append(left.pop())
         else:
-            merged_list.append(right[0])
-            right = right[1:]
-            r_len = r_len - 1
-    if l_len > 0:
+            merged_list.append(right.pop())
+    if left:
         merged_list.extend(left)
     else:
         merged_list.extend(right)
@@ -23,7 +19,7 @@ def mergesort(array):
     length = len(array)
     if length <= 1:
         return array
-    mid = int(length / 2)
+    mid = length // 2
     # this is black magic and stands in place of n assignments
     left = array[:mid]
     right = array[mid:]
